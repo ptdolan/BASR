55d5e91322e6ce7eaaab18de3cdf7151e2ea6835
Patrick Dolan <ptdolan@stanford.edu>
2020-04-28 12:09:10 -0700

    Updated with examples
    Example input TRIC fasta, alignments, trees

--- CodonAlignment.R
+++ CodonAlignment.R
@@ -1,5 +1,5 @@
-#Basic Alignment.R
-#Alignment of fa files. Usage (in command line): `Rscript BasicAlignment.R <myfasta.fa> [<other optional .fa's ... >]
+#CodonAlignment.R
+#Codon Alignment of fa files. Usage (in command line): `Rscript BasicAlignment.R <myfasta.fa> [<other optional .fa's ... >]
 library(Biostrings)
 library(DECIPHER)
 
@@ -8,8 +8,8 @@ inputSeqs<-readDNAStringSet(commandArgs(trailingOnly=TRUE))#Vector of FASTA file
 inputSeqs<-inputSeqs[]#Filter by some criteria. i
 names(inputSeqs)<-lapply(X = names(inputSeqs),function(X){paste(strsplit(X," ")[[1]],collapse= "_")}) #avoids some name parsing issues with other programs. 
 inputSeqs_Gapless<-RemoveGaps(inputSeqs)
-MAlign<-AlignSeqs(inputSeqs_Gapless,refinements=10)
+MAlign<-AlignTranslation(inputSeqs_Gapless,refinements=50)
 SMAlign<-StaggerAlignment(MAlign)
-writeXStringSet(SMAlign,filepath = "basicAln_output.aln")
+writeXStringSet(SMAlign,filepath = "codonAln_output.aln")
 
-system(command = "FastTree -nt -gtr < ./basicAln_output.aln > basicAln.tree")
+system(command = "FastTree -nt -gtr < ./codonAln_output.aln > codonAln.tree")
