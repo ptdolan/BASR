d4bae58b1055c4771c31e6ea0f7be46b5b609b3b
Patrick Dolan <ptdolan@stanford.edu>
2020-07-28 11:29:57 -0700

    Codon Alignment

--- Scripts/CodonAlignment.R
+++ Scripts/CodonAlignment.R
@@ -1,13 +1,27 @@
-#CodonAlignment.R
-#Codon Alignment of fa files. Usage (in command line): `Rscript BasicAlignment.R <myfasta.fa> [<other optional .fa's ... >]
-library(Biostrings)
-library(DECIPHER)
+#Basic Alignment.R
+#Alignment of fa files. Usage (in command line): `Rscript BasicAlignment.R <myfasta.fa> [<other optional .fa's ... >]
 
-inputSeqs<-readDNAStringSet(commandArgs(trailingOnly=TRUE))#Vector of FASTA files. 
+suppressMessages(library(Biostrings))
+suppressMessages(library(DECIPHER))
 
-inputSeqs<-inputSeqs[]#Filter by some criteria. i
-names(inputSeqs)<-lapply(X = names(inputSeqs),function(X){paste(strsplit(X," ")[[1]],collapse= "_")}) #avoids some name parsing issues with other programs. 
-inputSeqs_Gapless<-RemoveGaps(inputSeqs)
-MAlign<-AlignTranslation(inputSeqs_Gapless,refinements=50)
+inputList<-commandArgs(trailingOnly=TRUE)
+inputSeqs<-readDNAStringSet(inputList)#Vector of DNA FASTA files.
+
+inputSeqs<-RemoveGaps(inputSeqs)#Dealign any alignments for correct sequence length.
+
+MED=median(width(inputSeqs))#median length
+MAD=mad(width(inputSeqs))#median deviation
+
+prelength<-length(inputSeqs)
+#inputSeqs<-inputSeqs[abs(width(inputSeqs)-MED)<=(MAD*2)]#filter out long or short sequences (2 deviations away...)
+postlength<-length(inputSeqs)
+
+print(paste("Cut sequences from",prelength,"to",postlength,"."))
+
+inputSeqs<-inputSeqs[]#Filter by some criteria.
+names(inputSeqs)<-lapply(X = names(inputSeqs),function(X){paste(strsplit(X," ")[[1]],collapse= "_")}) #avoids some name parsing issues with other programs.
+
+MAlign<-AlignTranslation(processors = 8,inputSeqs,refinements=10)
 SMAlign<-StaggerAlignment(MAlign)
-writeXStringSet(SMAlign,filepath = "codonAln_output.aln")
+writeXStringSet(SMAlign,filepath = paste(inputList[1],"_codonAlign.aln",sep = ""))
+print(paste("Finished Alignment. Wrote",inputList[1],"_codonAlign.aln",sep=""))
